65d11d535282e80dccc5daaa065e16f102fb1489
rwinscot <rick.winscot@gmail.com>
2018-08-22 13:40:26 -0400

    JSON returned from icanhazdadjoke as local model

---
+++ Models/Joke.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+
+namespace WebApplication
+{
+    public class Joke
+    {
+        // https://icanhazdadjoke.com
+        // application/json
+        //
+        // {
+        // "id": "BQnyImOCIBd", 
+        // "joke": "How do hens stay fit? They always egg-ercise!", 
+        // "status": 200
+        // }
+
+        public string id { get; set; }
+        public string joke { get; set; }
+        public int WordCount { get; set; }
+
+
+        // Not sure how we can use this... exclude for now.
+
+        [JsonIgnore] public int status { get; set; }
+    }
+}
